c22654d548ef2eb9d299580efb40a84dd392acd9
Herbert Duarte <herbertduarte.santos@gmail.com>
2023-02-28 12:27:07 -0300

    add thanks page

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,7 +1,26 @@
 import styles from "./About.module.css";
+import { Link } from "react-router-dom";
 
 const About = () => {
-  return <section className={styles.aboutSection}>About</section>;
+  return (
+    <section className={styles.aboutSection}>
+      <h1  className={styles.title}>Olá, sou Clara!</h1>
+      <img  className={styles.image} src="/secont-img.jpg" alt="ana clara" />
+      <p className={styles.text}>
+        Uma verdadeira amante de fotografia, beleza e moda desde
+        muito pequena. Tenho 20 anos, curso Estética e Cosmética e futuramente
+        serei maquiadora profissional. Sempre fui extremamente vaidosa e gosto de
+        me arrumar para qualquer lugar. Tive uma conexão com a fotografia desde os
+        15 anos, quando meus pais me presentearam com uma câmera fotográfica.
+        Nunca tive vergonha alguma de pousar para fotos e logo fiz um curso de
+        modelo, concluí e recebi meu certificado. Já fiz alguns trabalhos como
+        modelo em Encruzilhada-Ba e Vitória da Conquista-Ba Sou muito
+        compromissada e dedicada em tudo que faço! Logo recebo avaliações
+        positivas devido minha competência com os trabalhos entregues.
+      </p>
+      <Link to='/works'>Conheça alguns dos meus trabalhos</Link>
+    </section>
+  );
 };
 
 export default About;
--- src/routes/Thanks.jsx
+++ src/routes/Thanks.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './Thanks.module.css'
 
 const Thanks = () => {
   return (
-    <div>Thanks</div>
+    <section className={styles.container}>
+      <h1 className={styles.title}>Obrigada por enviar uma mensagem!</h1>
+      <Link className={styles.link} to='/'>Voltar</Link>
+    </section>
   )
 }
 
