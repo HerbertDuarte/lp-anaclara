eece908d80b250298e0035b6b5574c324a4df9a8
Herbert Duarte <herbertduarte.santos@gmail.com>
2023-03-02 00:40:12 -0300

    c

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,14 +1,14 @@
-import './Slider.css'
+import styles from './Slider.module.css'
 
 export const Slider = () => {
 
   return (
-  <div className='sliderContainer'>
-    <div className='slider'>
-      <div className='slideDiv slide1'><img src="/slide1.jpeg" alt="Ana Clara" /></div>
-      <div className='slideDiv slide2'><img src="/slide2.jpeg" alt="Ana Clara" /></div>
-      <div className='slideDiv slide3'><img src="/slide3.jpeg" alt="Ana Clara" /></div>
-      <div className='slideDiv slide4'><img src="/slide4.jpeg" alt="Ana Clara" /></div>
+  <div className={styles.container}>
+    <div className={styles.slider}>
+      <div className={styles.slideDiv}><img src="/slide1.jpeg" alt="Ana Clara" /></div>
+      <div className={styles.slideDiv}><img src="/slide2.jpeg" alt="Ana Clara" /></div>
+      <div className={styles.slideDiv}><img src="/slide3.jpeg" alt="Ana Clara" /></div>
+      <div className={styles.slideDiv}><img src="/slide4.jpeg" alt="Ana Clara" /></div>
     </div>
   </div>
   )
