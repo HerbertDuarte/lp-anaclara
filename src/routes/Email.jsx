150f1162e970bd68e413899b1ea8ac371563f5a8
Herbert Duarte <herbertduarte.santos@gmail.com>
2023-03-02 16:12:14 -0300

    Update Email.jsx

--- src/routes/Email.jsx
+++ src/routes/Email.jsx
@@ -8,7 +8,7 @@ const Email = () => {
       <input className={styles.email} required type="email" name="email" placeholder='Digite seu email'></input>
       <textarea className={styles.message} required name="mensagem" id="mensagem" cols="30" rows="8" placeholder='Digite sua mensagem'></textarea>
       <input className={styles.button} type="submit" value="Enviar" />
-      {/* <input type="hidden" name="_next" value="https://anaclara-model-thanks.netlify.app"></input> */}
+      <input type="hidden" name="_next" value="https://anaclara-model.vercel.app/thanks"></input>
     </form>
   </section>
   )
