33a7c2c48dbe7da01683c3d2bd5f64b5a941921f
Herbert Duarte <herbertduarte.santos@gmail.com>
2023-03-02 12:17:43 -0300

    fix send message

--- src/routes/Email.jsx
+++ src/routes/Email.jsx
@@ -4,11 +4,11 @@ const Email = () => {
   return(
   <section>
     <h1 className={styles.title}>Formulário de Email</h1>
-    <form className={styles.form} action="https://formsubmit.co/c31ab4f2a9006434dba4f4629634cc3d" method="POST" >
+    <form className={styles.form} action="https://formsubmit.co/anaclaramelo10.acr@gmail.com" method="POST" >
       <input className={styles.email} required type="email" name="email" placeholder='Digite seu email'></input>
       <textarea className={styles.message} required name="mensagem" id="mensagem" cols="30" rows="8" placeholder='Digite sua mensagem'></textarea>
       <input className={styles.button} type="submit" value="Enviar" />
-      <input type="hidden" name="_next" value="/thanks"></input>
+      {/* <input type="hidden" name="_next" value="https://anaclara-model-thanks.netlify.app"></input> */}
     </form>
   </section>
   )
