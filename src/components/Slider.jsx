d93f8b9c23514a0608373d30b771ed35457772a2
Herbert Duarte <herbertduarte.santos@gmail.com>
2023-03-02 00:33:48 -0300

    ccommit

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,95 +1,18 @@
-import { useState } from "react";
+import './Slider.css'
 
-const slideStyles = {
-  width: "100%",
-  height: "100%",
-  borderRadius: "10px",
-  backgroundSize: "cover",
-  backgroundPosition: "center",
-};
-
-const rightArrowStyles = {
-  position: "absolute",
-  top: "50%",
-  transform: "translate(0, -50%)",
-  right: "32px",
-  fontSize: "45px",
-  color: "#fff",
-  zIndex: 1,
-  cursor: "pointer",
-};
-
-const leftArrowStyles = {
-  position: "absolute",
-  top: "50%",
-  transform: "translate(0, -50%)",
-  left: "32px",
-  fontSize: "45px",
-  color: "#fff",
-  zIndex: 1,
-  cursor: "pointer",
-};
-
-const sliderStyles = {
-  position: "relative",
-  height: "100%",
-};
-
-const dotsContainerStyles = {
-  display: "flex",
-  justifyContent: "center",
-};
-
-const dotStyle = {
-  margin: "0 3px",
-  cursor: "pointer",
-  fontSize: "20px",
-};
-
-const Slider = ({ slides }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const goToPrevious = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
-  };
-  const goToNext = () => {
-    const isLastSlide = currentIndex === slides.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-  };
-  const goToSlide = (slideIndex) => {
-    setCurrentIndex(slideIndex);
-  };
-  const slideStylesWidthBackground = {
-    ...slideStyles,
-    backgroundImage: `url(${slides[currentIndex].url})`,
-  };
+export const Slider = () => {
 
   return (
-    <div style={sliderStyles}>
-      <div>
-        <div onClick={goToPrevious} style={leftArrowStyles}>
-          ❰
-        </div>
-        <div onClick={goToNext} style={rightArrowStyles}>
-          ❱
-        </div>
-      </div>
-      <div style={slideStylesWidthBackground}></div>
-      <div style={dotsContainerStyles}>
-        {slides.map((slide, slideIndex) => (
-          <div
-            style={dotStyle}
-            key={slideIndex}
-            onClick={() => goToSlide(slideIndex)}
-          >
-            ●
-          </div>
-        ))}
-      </div>
+  <div className='sliderContainer'>
+    <div className='slider'>
+      <div className='slideDiv slide1'><img src="/slide1.jpeg" alt="Ana Clara" /></div>
+      <div className='slideDiv slide2'><img src="/slide2.jpeg" alt="Ana Clara" /></div>
+      <div className='slideDiv slide3'><img src="/slide3.jpeg" alt="Ana Clara" /></div>
+      <div className='slideDiv slide4'><img src="/slide4.jpeg" alt="Ana Clara" /></div>
     </div>
-  );
-};
+  </div>
+  )
+  
+}
 
 export default Slider;
