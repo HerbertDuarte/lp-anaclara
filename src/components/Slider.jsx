1e0c28e10fc0a21cab7c0f0a9442d76d94f337b8
Herbert Duarte <herbertduarte.santos@gmail.com>
2023-03-04 14:48:58 -0300

    change works

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,18 +1,17 @@
-import styles from './Slider.module.css'
+import styles from "./Slider.module.css";
 
-export const Slider = () => {
-
+export const Slider = ({ images }) => {
   return (
-  <div className={styles.container}>
-    <div className={styles.slider}>
-      <div className={styles.slideDiv}><img src="/slide1.jpeg" alt="Ana Clara" /></div>
-      <div className={styles.slideDiv}><img src="/slide2.jpeg" alt="Ana Clara" /></div>
-      <div className={styles.slideDiv}><img src="/slide3.jpeg" alt="Ana Clara" /></div>
-      <div className={styles.slideDiv}><img src="/slide4.jpeg" alt="Ana Clara" /></div>
+    <div className={styles.container}>
+      <div className={styles.slider}>
+        {images.map((image, index) => (
+          <div key={index} className={styles.slideDiv}>
+            <img src={image} alt="Ana Clara" />
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-  )
-  
-}
+  );
+};
 
 export default Slider;
--- src/routes/Works.jsx
+++ src/routes/Works.jsx
@@ -1,18 +1,81 @@
-import Slider from '../components/Slider'
-import styles from './Works.module.css'
+import Slider from "../components/Slider";
+import styles from "./Works.module.css";
 
 const Works = () => {
+  const slides1 = [
+    "/work1/slide1-1.jpeg",
+    "/work1/slide1-2.jpeg",
+    "/work1/slide1-3.jpeg",
+    "/work1/slide1-4.jpeg",
+  ];
 
+  const slides2 = [
+    "/work2/slide1.jpeg",
+    "/work2/slide2.jpeg",
+    "/work2/slide3.jpeg",
+    "/work2/slide4.jpeg",
+  ];
+
+  const slides3 =[
+    "/work3/slide1.jpg",
+    "/work3/slide2.jpg",
+    "/work3/slide3.jpeg",
+    "/work3/slide4.jpg"
+  ]
+
+  const slides4 = [
+    "/work4/slide1.jpg",
+    "/work4/slide2.jpg",
+    "/work4/slide3.jpg",
+    "/work4/slide4.jpg",
+  ];
   return (
     <section className={styles.container}>
       <h1 className={styles.title}>Meus trabalhos</h1>
-      <p className={styles.text}>Apresentação da nova linha de óculos <a href="https://www.maumau.com.br/oculos-femininos">Mau Mau</a>.</p>
-      <div>
-        <Slider/>
-      </div>
-      <p className={styles.description}>Trabalho feito para <a href="https://www.instagram.com/casademaria.presentes/">@casademaria.presentes</a> e <a href="https://www.instagram.com/dai.acessorios/">@dai.acessorios</a></p>
-    </section>
-  )
-}
 
-export default Works
+      <div>
+
+        <p className={styles.description}>
+          <span>Trabalho feito para </span>
+          <a href="https://www.instagram.com/casademaria.presentes/">
+            @casademaria.presentes
+          </a><span> e </span>
+          <a href="https://www.instagram.com/dai.acessorios/">
+            @dai.acessorios
+          </a>
+        </p>
+
+        <Slider images={slides1} />
+
+        <hr />{/* ------------------------------- */}
+
+
+        <p className={styles.description}>
+        Trabalho feito para loja de calçados <a href="https://www.instagram.com/iris_calcados.vca/">@iris_calcados.vca</a>
+        </p>
+
+        <Slider images={slides2} />
+        
+        <hr />{/* ------------------------------- */}
+
+        <p className={styles.description}>
+        <span>Trabalho feito para loja de roupas</span> <a href="https://www.instagram.com/betymodas/">@betymodas</a>
+        </p>
+        
+        <Slider images={slides3} />
+
+        <hr />{/* ------------------------------- */}
+
+
+        <p className={styles.description}>
+        <span>Trabalho feito para loja de roupas </span><a href="https://www.instagram.com/divastyleeee/">@divastyleeee</a>
+        </p>
+
+        <Slider images={slides4} />
+        
+      </div>
+    </section>
+  );
+};
+
+export default Works;
